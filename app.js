4cb11c945ac74c8d9c7562e6187b46ab46f98684
Behruz Bahodirov <gmod4252@gmail.com>
2026-03-28 15:16:37 +0300

    Question 2

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ function checkAlive (health) {
 console.log(checkAlive(0))
 console.log(checkAlive(4))
 console.log(checkAlive(-1))
+
+
+function addFive(num) {
+  var total = num + 5
+  return total
+}
+
+console.log(checkAlive(0))
+console.log(checkAlive(5))
+console.log(checkAlive(-5))
