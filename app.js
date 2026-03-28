78e487d024c91c0f6ed34e02fd631039cc14fd03
Behruz Bahodirov <gmod4252@gmail.com>
2026-03-28 15:55:23 +0300

    Question 3

--- app.js
+++ app.js
@@ -19,3 +19,20 @@ function addFive(num) {
 console.log(checkAlive(0))
 console.log(checkAlive(5))
 console.log(checkAlive(-5))
+
+function sumMix(x) {
+y = 0
+z = 0
+    let t = typeof(x)
+    for(key of x) {
+        if (t == String) {
+            y += +key
+        } else {
+            z += +key 
+        }
+    }
+    return y + z
+}
+console.log(sumMix([1,2,3,'3']))
+console.log(sumMix(['1',7,31,'3']))
+console.log(sumMix(['11',4,'3',3]))
